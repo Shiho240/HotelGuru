3664636a654f09ecdc631e85b55b614eeaf98a74
MasterShihoChief <MasterShihoChief240@gmail.com>
2013-09-25 16:52:49 -0700

    Removed test activity MainActivity. Made FirstUse the first activity ran by the app. Created new XML file for this activity. Will read in username and password and add to local database. Also reads in localization information and adds it to the preferences.

--- gen/com/sgs/hotelguru/R.java
+++ gen/com/sgs/hotelguru/R.java
@@ -24,14 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int pager=0x7f080000;
-        public static final int pager_title_strip=0x7f080001;
-        public static final int section_label=0x7f080002;
+        public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main_dummy=0x7f030001;
+        public static final int firstuse=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f070000;
