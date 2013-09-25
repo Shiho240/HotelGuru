09b3b40d293d1f9df1a77c58232b558f5d45b630
MasterShihoChief <MasterShihoChief240@gmail.com>
2013-09-25 14:06:47 -0700

    Fix bug in first time use

--- src/com/sgs/hotelguru/firstUseSetup.java
+++ src/com/sgs/hotelguru/firstUseSetup.java
@@ -1,14 +1,23 @@
 package com.sgs.hotelguru;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 
 public class firstUseSetup extends Activity{
 	
 	public static final String PREFS_NAME = "HotelGuru_Prefs_File";
+	public void firstuse(){
+		SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
+		
+		if(settings.getBoolean("firstBoot", true))
+				{
+			//FIRST BOOT DETECTED, IMPORT DATABASE AND QUERY FOR LOGIN INFORMATION
+				}
+		else
+		{
+			//NORMAL MODE CONTINUE TO MAIN ACTIVITY
+		}
+	}
 	
-	
-	
-	
-	
-
+		
 }
