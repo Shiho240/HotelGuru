511bdd99c047fe1ecf0131dc3fa0bd8df1c37c59
MasterShihoChief <MasterShihoChief240@gmail.com>
2013-09-25 18:20:38 -0700

    End of day commit. Program crashes on first run due to incomplete xml layout file. Complete tomorrow.

--- gen/com/sgs/hotelguru/R.java
+++ gen/com/sgs/hotelguru/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080003;
+        public static final int locale=0x7f080002;
         public static final int userName=0x7f080000;
         public static final int userPass=0x7f080001;
     }
