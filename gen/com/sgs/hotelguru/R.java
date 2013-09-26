b551fba4c3f3555402c2a9576307ea1ef1eabb6e
MasterShihoChief <MasterShihoChief240@gmail.com>
2013-09-25 17:57:40 -0700

    Added Username and password input to firstuse layout.

--- gen/com/sgs/hotelguru/R.java
+++ gen/com/sgs/hotelguru/R.java
@@ -24,7 +24,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int userName=0x7f080000;
+        public static final int userPass=0x7f080001;
     }
     public static final class layout {
         public static final int firstuse=0x7f030000;
@@ -35,9 +37,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int passHint=0x7f050006;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
         public static final int title_section3=0x7f050004;
+        public static final int uidHint=0x7f050005;
     }
     public static final class style {
         /** 
