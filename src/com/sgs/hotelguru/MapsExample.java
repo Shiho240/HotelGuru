8a98120e827d1f9582564628501ca8155efc4b06
Joshua Hunter <MasterShihoChief240@gmail.com>
2013-10-16 11:52:18 -0700

    Cortana saves the day :D

--- src/com/sgs/hotelguru/MapsExample.java
+++ src/com/sgs/hotelguru/MapsExample.java
@@ -32,10 +32,14 @@ public class MapsExample extends Activity {
 
 	View mainView;
     Button QM2_12002;
+    Button QM2_12081;
     Button buttonZoomIn;
     final float Zoom_max = 4f;
+    final float Zoom_min = 1f;
     private static final String TAG = "Die Karte";
     myDatabase db;
+	float myWorkX = 0;
+	float myWorkY = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,18 +48,19 @@ public class MapsExample extends Activity {
         mainView =findViewById(R.id.DeckLayout);
         db = new myDatabase(getApplicationContext());
 
-        //Button buttonZoomOut = (Button)findViewById(R.id.zoom_out);
+        Button buttonZoomOut = (Button)findViewById(R.id.zoom_out);
+        buttonZoomOut.setX(350);
         //Button buttonNormal = (Button)findViewById(R.id.reset);
         buttonZoomIn = (Button)findViewById(R.id.zoom_in);
         QM2_12002 = (Button)findViewById(R.id.QM2_12002);
-
-      /*buttonZoomOut.setOnClickListener(new OnClickListener() {
+        QM2_12081 = (Button)findViewById(R.id.QM2_12081);
+      buttonZoomOut.setOnClickListener(new OnClickListener() {
 
        @Override
        public void onClick(View v) {
-        zoom(0.5f,0.5f,new PointF(0,0));
+        zoom_out(new PointF(350,400));
        }
-      });
+      });/*
       buttonNormal.setOnClickListener(new OnClickListener() {
 
        @Override
@@ -63,22 +68,49 @@ public class MapsExample extends Activity {
         zoom(1f,1f,new PointF(0,0));
        }
       });*/
+      mainView.setOnTouchListener(new OnTouchListener(){
+
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MapsExample.this,"Setting zoom point to here",Toast.LENGTH_SHORT).show();
+				myWorkX = arg1.getX();
+				myWorkY = arg1.getY();
+				return true;
+			}
+  	});
         buttonZoomIn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                zoom(new PointF(0,0));
+            	
+
+            	
+            	
+                zoom(new PointF(myWorkX,myWorkY));
             }
         });
         QM2_12002.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            	float myWorkX = QM2_12002.getX();
-            	float myWorkY = QM2_12002.getY();
+            	//float myWorkX = QM2_12002.getX();
+            	//float myWorkY = QM2_12002.getY();
             	roomDataStruct myRoomData = db.getRoomData("Queen Mary 2", 12002);
                 Toast.makeText(MapsExample.this,myRoomData.getRoomNum()+" "+myRoomData.getRoomType()+" "+myRoomData.getRoomDeck(),
                         Toast.LENGTH_SHORT).show();
             }
+            
+        });
+        QM2_12081.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            	//float myWorkX = QM2_12081.getX();
+            	//float myWorkY = QM2_12081.getY();
+            	roomDataStruct myRoomData = db.getRoomData("Queen Mary 2", 12081);
+                Toast.makeText(MapsExample.this,myRoomData.getRoomNum()+" "+myRoomData.getRoomType()+" "+myRoomData.getRoomDeck(),
+                        Toast.LENGTH_SHORT).show();
+            }
+            
         });
         mainView.post(new Runnable() {
             // Post in the parent's message queue to make sure the parent
@@ -87,9 +119,12 @@ public class MapsExample extends Activity {
             public void run() {
                 // The bounds for the delegate view (an ImageButton
                 // in this example)
-                QM2_12002.setX(300);
-                QM2_12002.setY(400);
+                QM2_12002.setX(325);
+                QM2_12002.setY(92);
                 QM2_12002.setEnabled(true);
+                QM2_12081.setX(390);
+                QM2_12081.setY(370);
+                QM2_12081.setEnabled(true);
             }
         });
     }
@@ -101,14 +136,29 @@ public class MapsExample extends Activity {
         mainView.setPivotY(pivot.y);
         float workScaleX = mainView.getScaleX();
         float workScaleY = mainView.getScaleY();
-        float workButtonX = QM2_12002.getX();
+       /* float workButtonX = QM2_12002.getX();
         float workButtonY = QM2_12002.getY();
+        float workButtonX12081 = QM2_12081.getX();
+        float workButtonY12081 = QM2_12081.getY();*/
         if(workScaleX<Zoom_max)
         {
             mainView.setScaleX(workScaleX*2f);
             mainView.setScaleY(workScaleY*2f);
-            QM2_12002.setX(workButtonX/2f);
+            /*QM2_12002.setX(workButtonX/2f);
             QM2_12002.setY(workButtonY/2f);
+            QM2_12081.setX(workButtonX12081/2f);
+            QM2_12081.setY(workButtonY12081/2.45f);*/
         }
+    }
+    public void zoom_out(PointF pivot){
+    	 mainView.setPivotX(pivot.x);
+         mainView.setPivotY(pivot.y);
+         float workScaleX = mainView.getScaleX();
+         float workScaleY = mainView.getScaleY();
+         if(workScaleX>Zoom_min)
+         {
+             mainView.setScaleX(workScaleX/2f);
+             mainView.setScaleY(workScaleY/2f);
+         }
     }
 }
