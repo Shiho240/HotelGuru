81fcc975598e17e47c71c8bb6f5d4e638a27aaf8
MasterShihoChief <MasterShihoChief240@gmail.com>
2013-11-13 23:12:46 -0800

    Working Powershell Bulk SQLite Insert Script

--- assets/databases/insert.bat
+++
@@ -1,6 +0,0 @@
-Setlocal EnableDelayedExpansion
-for /l %%i in (12002, 2, 12010) do (
-echo "!status!"
-	SET /A "j = (((%%i - 12000)*15)+140)"
-	sqlite3 HotelGuru "INSERT INTO Rooms (Room_Num, Room_Type, Room_Desc, Room_Rating, Room_Deck, Ship_Name, Cruise_Line, Room_Interior, Room_Exterior, ButtonX, ButtonY) VALUES (%%i, "Forward Brittania Balcony Stateroom", "At any time of the day your luxury stateroom provides a welcome enclave of comfort and good taste. Sit and watch the world go by from your own private balcony.Your Stateroom Includes: 24-hour room service,Bathrobe and slippers,Nightly turndown service with pillow chocolate,Satellite TV with multi-language film and music channels,Refrigerator, safe, hair dryer,Daily shipboard newspaper,Half a bottle of Bon Voyage wine,Direct-dial telephone,Fruit basket (on request),220V British 3-pin and 110V 2-pin sockets", 4, 12, "Queen Mary 2", "Cunard", "www.google.com", "www.google.com", 460,%j% );"
-)
--- assets/databases/powerinsert.ps1
+++ assets/databases/powerinsert.ps1
@@ -1,5 +1,7 @@
-for($i=12002;$i -le 12010;i+=2)
+import-module SQLite
+mount-sqlite -name mydb -dataSource HotelGuru
+for($i=12002;$i -le 12010;$i+=2)
 {
-	$j = ((($i-12000)*15)+140);
-	.\sqlite3 HotelGuru INSERT INTO Rooms (Room_Num, Room_Type, Room_Desc, Room_Rating, Room_Deck, Ship_Name, Cruise_Line, Room_Interior, Room_Exterior, ButtonX, ButtonY) VALUES ($i, "Forward Brittania Balcony Stateroom", "At any time of the day your luxury stateroom provides a welcome enclave of comfort and good taste. Sit and watch the world go by from your own private balcony.Your Stateroom Includes: 24-hour room service,Bathrobe and slippers,Nightly turndown service with pillow chocolate,Satellite TV with multi-language film and music channels,Refrigerator, safe, hair dryer,Daily shipboard newspaper,Half a bottle of Bon Voyage wine,Direct-dial telephone,Fruit basket on request,220V British 3-pin and 110V 2-pin sockets", 4, 12, "Queen Mary 2", "Cunard", "www.google.com", "www.google.com", 460,$j );
+	$j = ((($i-12000)*15)+140)
+	new-item -path mydb:/Rooms -Room_Num $i -Room_Type "Forward Brittania Balcony Stateroom" -Room_Desc "At any time of the day your luxury stateroom provides a welcome enclave of comfort and good taste. Sit and watch the world go by from your own private balcony.Your Stateroom Includes: 24-hour room service,Bathrobe and slippers,Nightly turndown service with pillow chocolate,Satellite TV with multi-language film and music channels,Refrigerator, safe, hair dryer,Daily shipboard newspaper,Half a bottle of Bon Voyage wine,Direct-dial telephone,Fruit basket on request,220V British 3-pin and 110V 2-pin sockets" -Room_Rating 4 -Room_Deck 12 -Ship_Name "Queen Mary 2" -Cruise_Line "Cunard" -Room_Interior "www.google.com" -Room_Exterior "www.google.com" -ButtonX 460 -ButtonY $j 
 }
